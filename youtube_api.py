e477f7b348d48f965e6b35c18f16cf01813c90a1
Daniel-Velez <danielitio_2006@hotmail.com>
2024-06-14 21:18:14 -0500

    1.0.1

--- youtube_api.py
+++ youtube_api.py
@@ -368,7 +368,7 @@ class YouTubeDownloader(QMainWindow):
 
     def check_for_updates(self):
         try:
-            response = requests.get('https://github.com/Daniel-Velez/Youtube-Downloader/blob/main/version.txt')
+            response = requests.get('https://github.com/Daniel-Velez/Youtube-Downloader/main/version.txt')
             latest_version = response.text.strip()
             if latest_version != CURRENT_VERSION:
                 self.prompt_update(latest_version)
